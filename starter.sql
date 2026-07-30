e3399079c76a72fb95bd244ee593d5630111a3d1
prabhak-rvs <prabhak@rvsgroup.com>
2026-07-30 15:22:52 +0530

    Create starter.sql

---
+++ starter.sql
@@ -0,0 +1,21 @@
+-- ============================================
+-- SQL Assignment
+-- Create the Student table
+-- ============================================
+
+-- Use the existing database
+USE CollegeDB;
+
+-- TODO:
+-- Create the Student table with the following columns:
+--
+-- StudentID      Number(5)      Primary Key
+-- StudentName    VARCHAR(20)    NOT NULL
+-- DOB            DATE           NOT NULL
+-- Gender         VARCHAR(10)    NOT NULL
+-- DepartmentID   Number(5)      NOT NULL
+--
+-- Apply appropriate constraints:
+-- PRIMARY KEY
+-- UNIQUE
+-- NOT NULL
